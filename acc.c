2ee6dac984a9dfbb6231987e963225ed3e5551be
xadhrit <maharajrabel@gmail.com>
2021-06-28 18:00:01 -0400

    initial commit

---
+++ acc.c
@@ -0,0 +1,11 @@
+/*
+main file for acc compiler
+*/
+#include "acc.h"
+
+int main(){
+   printf("Initialising a C compiler\n");
+   return 0;
+}
+
+
---
+++ acc.h
@@ -0,0 +1,23 @@
+/*
+Main Header file for Adhrit C Compiler
+*/
+#include <stdio.h>
+#include <strings.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <assert.h>
+#include <unistd.h>
+#include <time.h>
+#include <glob.h>
+#include <errno.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdnoreturn.h>
+
+
+
+
