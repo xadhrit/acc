43263c4e72ba4c7621e39d7967e3762a1dba8a35
xadhrit <maharajrabel@gmail.com>
2021-07-14 16:14:31 -0400

    t

--- tokenize.c
+++ tokenize.c
@@ -343,11 +343,473 @@ static bool convert_pp_int(Token *tok){
    bool u = false;
 
    if (startswith(pos, "LLU") || startswith(pos, "LLu") || startswith(pos, "llU") || startswith(pos, "llu") ||  startswith(pos, "ULL") || startswith(pos, "Ull") || startswith(pos, "uLL") || startswith(pos, "ull")){
-      // 
+     p + 3;
+     l = u = true;
    }
+   else if (!strncasecmp(p, "lu", 2) || !strncase(p, "ul",2)){
+      p += 2;
+      l = u= true;
+   }
+   else if (startswith(p, "LL") || startswith(p, "ll", 2)){
+     p++;
+     l = true;
+   } else if (startswith(p, "L") || *p = 'l'){
+      p++;
+      l = true;
+   }
+   else if (*p = 'U' || *p == 'u'){
+       p++;
+       u = true;
+   }
+
+   if (p != tok->loc + tok->len){
+     return false;
+   }
+
+   // Infer a type
+   Type *ty;
+   if (base == 10){
+      if (l && u){
+         ty = ty_ulong;
+      }
+      else if (l){
+        ty = ty_long;
+      }
+      else if (u){
+        ty = (val >> 32) ? ty_ulong : ty_uint;
+      }
+      else {
+        ty = (val >> 31) ? ty_ulong : ty_int;
+      }
+   }
+   else {
+      if (l && u){
+         ty = ty_ulong;
+      }
+      else if (l){
+         ty = (val >> 63) ? ty_ulong : ty_long;
+      }
+      else if (u){
+         ty = (val >> 32) ? ty_ulong : ty_uint;
+      }
+      else if (val >> 63){
+         ty = ty_ulong;
+      }
+      else if (val >> 32){
+         ty = ty_long;
+      }
+      else if (val > 31){
+         ty = ty_uint;
+      }
+      else {
+        ty = ty_int;
+      }
+   }
+   tok->kind = TK_NUM;
+   tok->val = val;
+   tok->ty = ty;
+   return true;
+}
+
+// write a function that will convert preprocessing numer token to a regualar token
+static void convert_pp_number(Token *tok){
+   // try to parse as an integer constant;
+   if (convert_pp_int(tok)){
+     return;
+   }
+
+   char *end;
+   long double val = strtold(tok->loc, &end);
+
+   Type *ty;
+   if (*end == 'f' || *end == 'F'){
+      ty =ty_float;
+      end++;
+   }
+   else if (*end == 'l' || *end == 'L'){
+     ty =ty_ldouble;
+     end++;
+   }
+   else {
+     ty =ty_double;
+   }
+
+   if (tok->loc + tok->len != end){
+     error_tok(tok, "invalid numeric constant");
+   }
+
+   tok->kind =TK_NUM;
+   tok->fval = val;
+   tok->ty = ty;
+}
+
+void convert_pp_tokens(Token *tok){
+  for (Token *t = tok; t->kind != TK_EOF; t=t->next){
+     if (is_keyword(t)){
+        t->kind = TK_KEYWORD;
+     }
+     else if (t->kind == TK_PP_NUM){
+        convert_pp_number(t);
+     }
+  }
 }
 
 
+static void add_line_numbers(Token *tok){
+   char *p = current_file->constants;
+   int n = 1;
 
+   do {
+      if (p == tok->loc){
+        tok->line_no = n;
+        tok = tok->start;
+      }
+      if (*p == '\n'){
+         n++;
+      }
+   }
+   while(*p++); 
+}
 
+Token *tokenize_string_literal(Token *tok, Type *basety){
+   Token *t;
+   if (basety->size == 2){
+     t = read_utf16_string_literal(tok->loc, tok->loc);
+   }
+   else {
+     t = read_utf32_string_literal(tok->loc, tok->loc, basety);
+   }
+   t->next = tok->next;
+   return t;
+}
+
+// Tokenize a given string and returns new tokens
+Token *tokenize(File *file){
+   current_file = file;
+
+   char *p = file->constants;
+
+   Token head=  {};
+   Token *cur =&head;
+
+   at_bol = true;
+   has_space = false;
+
+   while(*p){
+      // skip line arguments
+      if (startswith(p, "//")){
+         p += 2;
+         while(*p != '\n'){
+           p++;
+         }
+         has_space = true;
+         continue;
+      }
+
+      // Skip block comments
+      if (startswith(p, "/*")){
+        char *q = strstr(p+2, "*/") ;
+        if (!q){
+          error_at(p, "unclosed block comment");
+        }
+         p = q+ 2;
+         has_space = true;
+         continue;
+      }
+
+      // Skip newline
+      if (*p == '\n'){
+         p++;
+         at_bol = true;
+         has_space = false;
+         continue;
+      }
+
+      // skip whitespace
+      if (isspace(*p)){
+         p++;
+         has_space = true;
+         continue;
+      }
+
+      // numeric literal
+      if (isdigit(*p) || (*p == '.' && isdigit(p[1]))){
+         char *q = p++;
+         for (;;){
+            if (p[0] && p[1] && strchr("eEpP", p[0]) && strchr("+-",p[1]) ){
+              p += 2;
+            }
+            else if (isalnum(*p) || *p == '.'){
+               p++;
+            }
+            else {
+              break;
+            }
+         }
+         cur = cur->next= new_token(TK_PP_NUM, p, q);
+         continue;
+      }
+
+     // String literal
+     if (*p == '"'){
+        cur = cur->next = read_string_literal(p, p);
+        p += cur->len;
+        continue;
+     }
+
+     // UTF-8 String literal
+     if (startswith(p, "u8\"")){
+        cur = cur->next = read_string_literal(p, p + 2);
+        p += cur->len;
+        continue;
+     }
+
+     // UTF-16 String literal
+     if (startswith(p, "u\"")){
+         cur = cur->next = read_utf16_string_literal(p, p+1);
+         p += cur->len;
+         continue;
+     }
+
+     // Wide string literal
+     if (startswith(p, "L\"")){
+        cur =cur->next = read_utf32_string_literal(p, p+1, ty_int);
+        p += cur->len;
+        continue;
+     }
+
+     // UTF32 string literal
+     if (startswith(p, "U\"")){
+        cur = cur->next =read_utf32_string_literal(p, p+1, ty_uint);
+        p += cur->len;
+        continue;
+     }
+
+     // Character literal
+     if (*p == '\''){
+       cur  = cur->next = read_char_literal(p, p, ty_int);
+       cur->val = (char)cur->val;
+       p += cur->len;
+       continue;
+     }
+
+     // utf-16 character literal
+     if (startswith(p, "u'")){
+        cur = cur->next = read_char_literal(p, p+1, ty_ushort);
+        cur->val &=  0xffff;
+        p += cur->len;
+        continue;
+     }
+    
+     // Wide character literal
+     if (startswith(p, "L'")){
+        cur = cur->next =read_char_literal(p, p+1, ty_int);
+        p += cur->len;
+        continue;
+     }
+
+     // utf-32 character literal
+     if (startswith(p, "U'")){
+        cur = cur->next = read_char_literal(p, p+1, ty_uint);
+        p += cur->len;
+        continue;
+     }
+
+     // Identifier or keyword
+     int ident_len = read_ident(p);
+     if (ident_len){
+        cur = cur->next = new_token(TK_IDENT, p, p+ident_int);
+        p  += cur->len;
+        continue;
+     }
+
+     //punctuations
+     int punct_len = read_punct(p);
+     if (punct_len){
+        cur = cur->next = new_token(TK_PUNCT, p, p+ punct_len);
+        p += cur->len;
+        continue;
+     }
+     error_at(p, "invalid token");
+   }
+
+   cur = cur->next = new_token(TK_EOF, p, p);
+   add_line_number(head.next);
+   return head.next;
+}
+
+// returns the constants of a given file
+static char *read_file(char *path){
+   FILE *fp;
+
+   if (strcmp(path, "-") == 0){
+     // by conversion
+     fp = stdin;
+   }
+   else {
+      fp = fopen(path, "r");
+      if (!fp){
+           return NULL;
+         }
+      }
+
+      char *buf;
+      size_t buflen;
+      FILE *out = open_memstream(&buf, &buflen);
+
+      // Read the entire file
+      for (;;){
+       char buf2[4096];
+       int n = fread(buf2, 1, sizeof(buf2), fp);
+       if (n==0){
+          break;
+       }
+       fwrite(buf2, 1, n , out);
+      }
+
+      if (fp != stdin){
+         fclose(fp);
+      }
+
+      // Make sure that the last line is properly terminated
+      fflush(out);
+      if (buflen == 0 || buf[len -1 ] != '\n'){
+         fputc('\n', out);
+      }
+      fputc('\0', out);
+      fclose('\0', out);
+      fclose(out);
+      return buf;
+   }
+}
+
+File **get_input_files(void){
+   return input_files;
+}
+
+File *new_file(char *name, int file_no, char *constants){
+   File *file =calloc(1, sizeof(File));
+   file->name = name;
+   file->display_name = name;
+   file->file_no = file_no;
+   file->contents= contents;
+   return file;
+}
+
+// replaces \r or \r\n with \n
+static void canonicalize_newline(char *p){
+    int i = 0;
+    int j = 0;
+    while (p[i]){
+       if (p[i] == '\r' && p[i+1] == '\n' ){
+          i += 2;
+          p[j++] = '\n';
+       }
+       else if (p[i] == '\r'){
+          i++;
+          p[j++]= '\n';
+
+       }
+       else {
+          p[j++] = p[i++];
+       }
+    }
+
+    p[j] = '\0';
+}
+
+static void remove_blackslash_newline(char *p){
+   int i=0, j = 0, n = 0;
+   while (p[i]){
+      if (p[i] == '\\' && p[i+1] == '\n'){
+         i += 2;
+         n++;
+      }
+      else if (p[i] == '\n'){
+        p[j++] = p[i++];
+        for (; n > 0; n--){
+           p[j++]  = '\n';
+
+        }
+      }
+      else {
+         p[j++] = p[i++];
+      }
+   }
+   for (; n > 0 ; n--){
+      p[j++]  = '\n';
+   }
+   p[j] = '\0';
+}
+
+static uint32_t read_universal_char(char *p, int len){
+   uint32_t c = 0;
+   for (int i = 0; i < len ; i++)
+   {
+      if (!isxdigit(p[i])){
+        return 0;
+      }
+      c = (c << 4) | from_hex(p[i]);
+   }
+
+   return c;
+}
+
+// replace \u or \U escape sequences with corresponding UTF-8 bytes
+static void convert_universal_chars(char *p){
+   char *p = p;
+
+   while (*p){
+     if (startswith(p, "\\u")){
+        uint32_t c  =read_universal_char(p+2, 4);
+        if (c){
+          p += 6;
+          q += encode_utf8(q, c);
+        }
+        else {
+          *q++ = *p++;
+        }
+      }
+   else if (startswith(p, "\\U")){
+       uint32_t c = read_universal_char(p+2, 8);
+       if (c){
+          p += 10;
+          q += encode_utf8(q, c);
+
+       }else {
+          *q++ = *p++;
+       }
+   }
+   else if (p[0] == '\\'){
+       *q++ = *p++;
+       *q++ = *p++;
+   } else {
+      *q++= *p++;
+   }
+ }
+  *q = '\0';
+} 
+
+Token *tokenize_file(char *path){
+   char *p = read_file(path);
+   if (!p){
+      return NULL;
+   }
+
+   if (!memcmp(p, "\xef\xbb\xbf", 3)){
+      p += 3;
+   }
+   canonicalize_newline(p);
+   remove_backslash_newline(p);
+   convert_universal_chars(p);
+
+   static int file_no;
+   File *file = new_file(path, file_no + 1, p);
+   intput_files = realloc(input_files, sizeof(char *) * (file_no + 2));
+   input_files[file_no] = file;
+   input_files[file_no + 1]  = NULL;
+   file_no++;
+
+   return tokenize(file);
+}
 
