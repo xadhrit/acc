eb076426670a2ceb526f682b65249f2fac6b168d
xadhrit <maharajrabel@gmail.com>
2021-07-19 02:52:57 -0400

    preprocesser

--- preprocessing.c
+++ preprocessing.c
@@ -751,6 +751,456 @@ static char *read_include_pathname(Token **rest, Token *tok, bool *is_dquote){
     error_tok(tok, "expected a filename");
 }
 
+/*
+ go thorugh the portion of #ifndef....#endif
+*/
+
+static char *detect_include_guard(Token *tok){
+   // Detect the first two lines
+   if (!is_hash(tok) || !equal(tok->next, "ifndef")){
+      return NULL;
+   }
+   tok = tok->next->next;
+
+   if (tok->kind != TK_IDENT){
+       return NULL;
+   }
+
+   char *macro = strndup(tok->loc , tok->len);
+   tok = tok->next;
+
+   if (!is_hash(tok) || !equal(tok->next, "define")){
+     return NULL;
+   }
+
+   // Read until EOF
+   while (tok->kind != TK_EOF){
+      if (!is_hash(tok)){
+          tok = tok->next;
+          continue;
+      }
+
+      if (equal(tok->next,"endif") && tok->next->next->kind == TK_EOF ){
+          return macro;
+      }
+      if (equal(tok, "if") || equal(tok, "ifdef") || equal(tok, "ifndef")){
+           tok = skip_cond_incl(tok->next);
+      }
+      else {
+          tok = tok->next;
+      }
+   }
+   return NULL;
+}
+
+static Token *include_file(Token *tok, char *path, Token *filename_tok){
+    // Check for "#pragma once"
+    if (hashmap_get(&pragma_once, path)){
+        return tok;
+    }
+
+    static HashMap include_guards;
+    char *guard_name = hashmap_get(&include_guards, path);
+    if (guard_name && hashmap_get(&macros, guard_name)){
+        return tok;
+    }
+
+    Token *tok2 = tokenize_file(path);
+    if (!tok2){
+       error_tok(filename_tok, "%s: cannnot open file: %s", path, strerror(errno));
+    }
+    return append(tok2, tok);
+}
+
+// Read #line arguments
+static void read_line_marker(Token **rest, Token *tok){
+   Token *start = tok;
+   tok = preprocess(copy_line(rest, tok));
+
+   if (tok->kind != TK_NUM || tok->ty->kind != TY_INT){
+      error_tok(tok, "invalid line marker");
+   }
+   start->file->line_delta = tok->val - start->line_no;
+
+   tok = tok->next;
+   if (tok->kind == TK_EOF){
+      return;
+   }
+
+   if (tok->kind != TK_STR){
+      error_tok(tok, "filename expected!");
+   }
+   start->file->display_name = tok->str;
+}
+
+// Visit all tokens in `tok` while evaluating preprocessing macros and directives
+static Token *preprocess2(Token *tok){
+    Token head = {};
+    Token *cur = &head;
+
+    while (tok->kind != TK_EOF){
+       // If it is a macro, expand it
+       if (expand_macro(&tok, tok)){
+          continue;
+       }
+
+       // not "#" then 
+       if (!is_hash(tok)){
+          tok->line_delta = tok->file->line_delta;
+          tok->filename = tok->file->display_name;
+          cur = cur->next = tok;
+          tok = tok->next;
+          continue;
+       }
+
+       Token *start = tok;
+       tok = tok->next;
+
+       if (equal(tok, "include")){
+           bool is_dquote;
+           char *filename = read_include_filename(&tok, tok->next, &is_dquote);
+           if (filename[0] != '/' && is_dquote ){
+               char *path = format("%s:%s", dirname(strdup(start->file->name)), filename);
+               if (file_exists(path)){
+                   tok = include_file(tok, path, start->next->next);
+                   continue;
+               }
+           }
+
+           char *path = search_include_paths(filename);
+           tok = include_file(tok, path ? path : filename, start->next->next );
+           continue;
+       }
+
+       if (equal(tok, "include_next")){
+           read_macro_definition(&tok, tok->next);
+           continue;
+       }
+
+       if (equal(tok, "undef")){
+          tok = tok->next;
+          if (tok->kind != TK_IDENT){
+             error_tok(tok, "macro name must be identifier");
+
+          }
+          undef_macro(strndup(tok->loc, tok->next));
+          tok = skip_line(tok->len);
+          continue;
+       }
+
+       if (equal(tok, "if")){
+          long val = eval_const_epxr(&tok, tok);
+          push_cond_incl(start, val);
+          if (!val){
+             tok = skip_cond_incl(tok);
+          }
+          continue;
+       }
+
+       if (equal(tok, "ifdef")){
+          bool defined = find_macro(tok->next);
+          push_cond_incl(tok, defined);
+          tok = skip_line(tok->next->next);
+          if (!defined){
+             tok = skip_cond_incl(tok);
+          }
+          continue;
+       }
+
+       if (equal(tok,"elif")){
+          if (!cond_incl || cond_incl->ctx == IN_ELSE){
+             error_tok(start, "stray #endif");
+          }
+          cond_incl->ctx = IN_ELIF;
+
+          if (!cond_incl->included && eval_const_expr(&tok, tok)){
+              cond_incl->included = true;
+          }
+          else {
+             tok = skip_cond_incl(tok);
+          }
+          continue;
+       }
+
+       if (equal(tok, "else")){
+          if (!cond_incl || cond_incl->ctx == IN_ELSE){
+              error_tok(start, "stray #else");
+          }
+          cond_incl->ctx = IN_ELSE;
+          tok = skip_line(tok->next);
+
+          if (cond_incl->included){
+              tok = skip_cond_incl(tok);
+          }
+          continue;
+       }
+
+       if (equal(tok, "endif")){
+           if (!cond_incl){
+               error_tok(start, "stray, #endif");
+           }
+           cond_incl = cond_incl->next;
+           tok = skip_line(tok->next);
+           continue;
+       }
+
+       if (equal(tok, "line")){
+           read_line_marker(&tok, tok->next);
+           continue;
+       }
+
+       if (tok->kind == TK_PP_NUM){
+          read_line_marker(&tok, tok);
+          continue;
+       }
+
+       if (equal(tok, "pragma") && equal(tok->next, "once") ){
+          hashmap_put(&pragma_once, tok->file->name, (void *)1);
+          tok = skip_line(tok->next->next);
+          continue;
+       }
+
+       if (equal(tok, "pragma")){
+          do{
+             tok = tok->next;
+
+          } while(!tok->at_bol);
+          continue;
+       }
+
+       if (equal(tok, "error")){
+           error_tok(tok, "error");
+       }
+
+       if (tok->at_bol){
+           continue;
+       }
+
+       error_tok(tok, "Invalid preprocessor directive");
+    }
+
+    cur->next = tok;
+    return head.next;
+}
+
+void define_macro(char *name, char *buf){
+   Token *tok = tokenize(new_file("<built-in>", 1, buf));
+   add_macro(name, true, tok);
+}
+
+void undef_macro(char *name){
+   hashmap_delete(&macros, name);
+}
+
+static Macro *add_buitin(char *name, macor_handler_fn * fn){
+    Macro *m = add_macro(name, true, NULL);
+    m->handler = fn;
+    return m;
+}
+
+static Token *file_macro(Token *tmpl){
+    while (tmpl->origin){
+       tmpl = tmpl->origin;
+    }
+    return new_str_token(tmpl->file->display_name, tmpl);
+}
+
+static Token *line_macro(Token *tmpl){
+   while (tmpl->origin){
+      tmpl = tmpl->origin;
+   }
+   int i = tmpl->line_no + tmpl->file->line_delta;
+   return new_num_token(i, tmpl);
+}
+
+// __COUNTER__ 0 to increment
+static Token *counter_macro(Token *tmpl){
+   static int i=0;
+   return new_num_token(i++, tmpl);
+}
+
+// __TIMSTAMP__ showing last time when file was edited
+// format : Fri, Jul 24 01:32:50 2020
+static Token *timestamp_macro(Token *tmpl){
+   struct stat st;
+   if (stat(tmpl->file->name, &st) != 0){
+      return new_str_token("??? ??? ?? ??:??:?? ????", tmpl);
+   }
+
+   char buf[30];
+   ctime_r(&st, st_mtime, buf);
+   buf[24] = '\0';
+   return new_str_token(buf, tmpl);
+}
+
+// __DATE__ is expanded to the current date e.g. 
+static char *format_date(struct tm *tm){
+   static char mon[][4] = {
+      "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+      "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+   };
+   return format("\%s %2d %d\"", mon[tm->tm_mon], tm->tm_mday, tm->tm_year + 1900);
+}
+
+// __TIME__ is expanded to current time 
+static char *format_time(struct tm *tm){
+   return format("\%02d:%02d:%02d\"", tm->tm_hour, tm->tm_min, tm->tm_sec);
+}
+
+void init_macros(void){
+   // Define predefined macors
+   define_macro("_LP64", "1");
+   define_macro("__C99_MACRO_WITH_VA_ARGS", "1");
+   define_macro("__ELF__", "1");
+   define_macro("__LP64__", "1");
+   define_macro("__SIZEOF_DOUBLE__", "8");
+   define_macro("__SIZEOF_FLOAT__", "4");
+   define_macro("__SIZEOF_INT__", "4");
+   define_macro("__SIZEOF_LONG_DOUBLE__", "8");
+   define_macro("__SIZEOF_LONG_LONG__", "8");
+   define_macro("__SIZEOF_LONG__", "8");
+   define_macro("SIZEOF_POINTER__", "8");
+   define_macro("SIZEOF_PTRDIFF_T__", "8");
+   define_macro("SIZEOF_SHORT__", "2");
+   define_macro("SIZEOF_SIZE_T__", "8");
+   define_macro("__SIZE_TYPE__", "unsigned long");
+   define_macro("__STDC_HOSTED__", "1");
+   define_macro("__STDC_NO_COMPLEX__", "1");
+   define_macro("__STDC_UTF_16__", "1");
+   define_macro("__STDC_UTF_32__", "1");
+   define_macro("__STDC_VERSION__", "201112L");
+   define_macro("__STDC__", "1");
+   define_macro("__USER_LABEL_PREFIX__", "");
+   define_macro("__alignof__", "_Alignof");
+   define_macro("__amd64", "1");
+   define_macro("__amd64__", "1");
+   define_macro("__acc__", "1");
+   define_macro("__const__", "const");
+   define_macro("__gnu_linux__", "1");
+   define_macro("__inline__", "inline");
+   define_macro("__signed__", "signed");
+   define_macro("__typeof__", "typeof");
+   define_macro("__unix", "1");
+   define_macro("__unix__", "1");
+   define_macro("__volatile__", "volatile");
+   define_macro("_x86_64", "1");
+   define_macro("__x86_64__", "1");
+   define_macro("linux", "1");
+   define_macro("unix", "1");
+
+   add_builtin("__FILE__",file_macro);
+   add_builtin("__LINE__", line_macro);
+   add_builtin("__COUNTER__", counter_macro);
+   add_builtin("__TIMESTAMP__",timestamp_macro);
+   add_builtin("__BASE_FILE__",base_file_macro);
+
+   time_t now = time(NULL);
+   struct tm *tm = localtime(&now);
+   define_macro("__DATE__", format_date(tm));
+   define_macro("__TIME__", format_time(tm));
+}
+
+typedef enum{
+   STR_NONE, STR_UTF8, STR_UTF16, STR_UTF32, STR_WIDE,
+} StrType; 
+
+static StrType getStrType(Token *tok){
+    if (!strcmp(tok->loc, "u8")){
+       return STR_UTF8;
+    }
+
+    switch(tok->loc[0]){
+       case '"': return STR_NONE;
+       case 'u': return STR_UTF8;
+       case 'U': return STR_UTF32;
+       case 'L': return STR_WIDE;
+    }
+    unreachable();
+}
+
+// Concatenate adjacent string literals into a single string literal as per the C spec.
+static void join_adjacent_string_literals(Token *tok){
+   // First pass: If regular string literals are adjacent then join them and string literals, regular string literals are converted to a wide and type before concatenation. In this pass, we do the conversion
+   for (Token *tok1 = tok; tok1->kind != TK_EOF){
+      if (tok1->kind != TK_STR || tok1->next->kind != TK_STR){
+          tok1 = tok1->next;
+          continue;
+      }
+
+      StrType kind = getStrType(tok1);
+      Type *basety = tok1->ty->base;
+
+      for (Token *t = tok1->next; t->kind == TK_STR; t = t->next){
+          StrType k = getStrType(t);
+          if (kind == STR_NONE){
+             kind = k;
+             basety = t->ty->base;
+          } else if(k != STR_NONE && kind != k) {
+             erorr_tok(t, "unsupported non-standard concatenation of string literals");
+
+          }
+      }
+      if (basety->size > 1){
+         for (Token *t = tok1; t->kind == TK_STR; t= t->next){
+            if (t->ty->base->size == 1){
+               *t = *tokenize_string_literal(t, basety);
+            }
+         }
+      }
+      while(tok1->kind == TK_STR){
+         tok1 = tok1->next;
+      }
+   }
+
+   // Second Pass: concatenate adjancent string literals
+   for (Token *tok1 = tok; tok1->kind != TK_EOF;){
+      if (tok1->kind != TK_STR || tok1->next->kind != TK_STR){
+         tok1 = tok1->next;
+         continue;
+      }
+
+      Token *tok2 = tok1->next;
+      while(tok2->kind == TK_STR){
+         tok2 = tok2->next;
+      }
+
+      int len = tok1->ty->array_len;
+      for (Token *t = tok1->next; t != tok2; t = t->next){
+          len = len + t->ty->array_len - 1;
+      }
+
+      char *buf = calloc(tok1->ty->base->size, len);
+
+      int i = 0;
+      for (Token *t = tok1; t != tok2; t = t->next ){
+         memcpy(buf + i, t->str, t->ty->size);
+         i = i + t->ty->size - t->ty->base->size;
+      }
+
+      *tok1 = *copy_token(tok1);
+      tok1->ty = array_of(tok1->ty->base, len);
+      tok1->str = buf;
+      tok1->next = tok2;
+      tok1 = tok2;
+   }
+}
+// entry point function of preprocessing analysis
+Token *preprocess(Token *tok){
+   tok = preprocess2(tok);
+   if (cond_incl){
+      error_tok(cond_incl->tok, "unterminated conditional directive");
+
+   }
+   convert_pp_tokens(tok);
+   join_adjacent_string_literals(tok);
+
+   for (Token *t = tok; t; t = t->next){
+      t->line_no += t->line_delta;
+   }
+   return tok;
+}
+
+
 
 
 
