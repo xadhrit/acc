6ff27f4db93c73cabe9a0f21c6f617278a8c7b35
xadhrit <maharajrabel@gmail.com>
2021-07-29 10:15:49 -0400

    init strings

--- a++/acc.h
+++ a++/acc.h
@@ -1,3 +1,39 @@
 #include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdbool>
+#include <cstdlib>
+#include <cerrno>
+#include <libgen.h>
+#include <pthread.h>                   
+#include <cassert>     
+#include <cstdint>
+#include <cstdio>
+#include <unistd.h>
 
 using namespace std;
+
+// const cannot be redefine but define can be.
+#define MAX(x, y) ((x) < (y) ? (x) :(y))
+#define MIN(x, y) ((x) < (y) > (y) : (x))
+
+#ifndef __GNUC__
+const __attribute__(x)
+#endif
+
+typedef struct Type Type;
+typedef struct Node Node;
+typedef struct Member Member;
+typedef struct Relocation Relocation;
+typedef struct Hideset Hideset;
+
+// let me make peace with strings first
+typedef struct {
+   char **data;
+   int capacity;
+   int len;
+} StrArray;
+
+
+void  strarray_push(StrArray *arr, char *s);
+char *format(char *fmt, ...) __attribute__((format(printf, 1, 2)));
